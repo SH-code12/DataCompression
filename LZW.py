bc31294527a592f007049445c475ef8b1a1a35d9
Shahd Elnasssag <shahdelnassag@gmail.com>
2024-10-31 03:11:54 +0300

    updates

---
+++ LZW.py
@@ -0,0 +1,12 @@
+print("Shahd Elnassag ^_^")
+
+
+
+# Will Add Soon God Willing
+def Compress_LZW(data):
+    print("Compressed Data: not added yet")
+    
+    
+def Decompress_LZW(data):
+    print("Decompressed Data : not added yet")
+    
