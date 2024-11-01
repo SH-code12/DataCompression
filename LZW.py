c81b182b0fe2644c62f9c630246faca7034c1ab1
Shahd Elnasssag <shahdelnassag@gmail.com>
2024-11-01 13:53:18 +0200

    Add compressed function of technique LZW but without deal with files

--- LZW.py
+++ LZW.py
@@ -4,9 +4,37 @@ print("Shahd Elnassag ^_^")
 
 # Will Add Soon God Willing
 def Compress_LZW(data):
-    print("Compressed Data: not added yet")
+    # table store ascii and another new chars 
+    dictinary_table = {}
+    for i in range(256):
+        dictinary_table[chr(i)] = i
+        
+    startcode = 256
+    # array store compressed data
+    compressedData = []
+    currentSeq = ""
+    
+    for nextSymbol in data:
+        newSeq = currentSeq + nextSymbol
+        if newSeq in dictinary_table:
+            currentSeq = newSeq
+        else:
+            compressedData.append(dictinary_table[currentSeq])
+            dictinary_table[newSeq] = startcode
+            startcode += 1
+            currentSeq = nextSymbol
+            
+    if currentSeq:
+        compressedData.append(dictinary_table[currentSeq])
+        
+    return compressedData
     
     
 def Decompress_LZW(data):
     print("Decompressed Data : not added yet")
+    
+print("Test Function Compressed LZW techniques ^_^")
+testData = input("Enter data to compressed it: ")
+print(Compress_LZW(testData))
+
     
--- ReadMe.md
+++ ReadMe.md
@@ -5,3 +5,11 @@
    1. Compressed Data
 
    2. Decoressed Data  
+
+   - Implementation For LZW Technique
+
+   1. Compressed Data
+
+   2. Decoressed Data 
+
+   3. Deal with files (input,output)
