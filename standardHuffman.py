6cad1949a9e592c80d1ac79e9740cba154eb865b
Shahd Elnasssag <shahdelnassag@gmail.com>
2024-11-25 14:58:27 +0200

    Add compress function

--- standardHuffman.py
+++ standardHuffman.py
@@ -1,48 +1,113 @@
-# 1. calculate propability
-
+# 1. Calculate probabilities
 def calculate_probabilities(data):
     frequencies = {}
-    
     for char in data:
         if char in frequencies:
             frequencies[char] += 1
         else:
             frequencies[char] = 1
 
-    total_Length = len(data)
-    
+    total_length = len(data)
     probabilities = {}
-    
     for char, freq in frequencies.items():
-        probabilities[char] = freq / total_Length
-            
+        probabilities[char] = freq / total_length
     return probabilities
 
 
+# Node class for Huffman tree
+class Node:
+    def __init__(self, char, freq):
+        self.char = char
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+
+# Build Huffman tree
+def sum_Probabilities(probabilities):
+    nodes = []
+
+    for char, freq in probabilities.items():
+        nodes.append(Node(char, freq))
+
+    while len(nodes) > 1:
+        # Sort the nodes by frequency
+        for i in range(len(nodes)):
+            for j in range(i + 1, len(nodes)):
+                if nodes[i].freq > nodes[j].freq:
+                    nodes[i], nodes[j] = nodes[j], nodes[i]
+
+        left = nodes.pop(0)
+        right = nodes.pop(0)
+
+        # Create a new node
+        newNode = Node(None, left.freq + right.freq)
+        newNode.left = left
+        newNode.right = right
+
+        nodes.append(newNode)
+
+    return nodes[0]  
+
+
+# Generate Huffman codes
+def create_codes(node, current_code="", codes={}):
+    if node is None:
+        return codes
+
+    if node.char is not None:  
+        codes[node.char] = current_code
+    else:
+        create_codes(node.left, current_code + "0", codes)
+        create_codes(node.right, current_code + "1", codes)
+
+    return codes
+
+
+# Compress the data
+def compress(data, codes):
+    compressed_data = ""
+    for char in data:
+        compressed_data += codes[char]
+    return compressed_data
+
+
+# Decompress the data
+def decompress(compressed_text, codes):
+    reverse_codes = {}
+    for key, value in codes.items():
+        reverse_codes[value] = key
+
+    pointer = 0
+    current_code = ""
+    original_text = ""
+
+    while pointer < len(compressed_text):
+        current_code += compressed_text[pointer]
+        if reverse_codes.get(current_code) is not None:
+            original_text += reverse_codes[current_code]
+            current_code = ""
+        pointer += 1
+
+    return original_text
+
+
 data = "bbbacacbcd"
+
 probabilities = calculate_probabilities(data)
 print("Character Probabilities:")
 for char, prob in probabilities.items():
     print(f"'{char}': {prob:.4f}")
 
+root = sum_Probabilities(probabilities)
 
-def decompress (compressedText,diction):
-    dict = {}
-    for key,value in diction.items():
-        dict[value] = key
-    pointer  =0
-    character =""
-    original_text =""
-    while(pointer<len(compressedText)):
-        character += compressedText[pointer]
-        if(dict.get(character) is not None):
-            original_text+= dict[character]
-            character=""
-        pointer+=1
-    return original_text
+huffman_codes = create_codes(root)
+print("\nHuffman Codes:")
+for char, code in huffman_codes.items():
+    print(f"'{char}': {code}")
 
+compressed_data = compress(data, huffman_codes)
+print(f"\nCompressed Data: {compressed_data}")
 
-print(decompress("0111011010",{'A':"0",'B':"11",'C':"10"}))
-
-            
-            
+decompressed_data = decompress(compressed_data, huffman_codes)
+print(f"Decompressed Data: {decompressed_data}")
